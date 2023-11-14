f39d4ec18fc66e8a13c3bb2d98dc0344b09bdbc5
Spolito24 <baptisterenier69@gmail.com>
2023-11-14 14:41:10 +0100

    add task form done

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Task } from '../model/Task';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.scss']
+  styleUrls: ['./add-task.component.scss'],
 })
 export class AddTaskComponent {
+  //j'instancie le model de données
+  task: Task = new Task('', '', false);
 
+  constructor() {}
+
+  //j'affiche le résultat du formulaire dans la console
+  onSubmit() {
+    console.log(this.task);
+  }
 }
